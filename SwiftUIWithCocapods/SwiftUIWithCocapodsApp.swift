2afb60800c85953d14a7fcd9a06111757da0f9d9
safarsafarov <safarov.safar@hotmail.com>
2020-10-08 15:49:11 +0800

    start

---
+++ SwiftUIWithCocapods/SwiftUIWithCocapodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithCocapodsApp.swift
+//  SwiftUIWithCocapods
+//
+//  Created by Safar Safarov on 2020/10/8.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithCocapodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
