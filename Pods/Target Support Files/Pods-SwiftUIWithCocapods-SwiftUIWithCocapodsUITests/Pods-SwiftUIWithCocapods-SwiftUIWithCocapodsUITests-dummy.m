01a0f92bfd238dc034eb4a29b92c1eac04b6aa82
safarsafarov <safarov.safar@hotmail.com>
2020-10-10 10:28:59 +0800

    start

---
+++ Pods/Target Support Files/Pods-SwiftUIWithCocapods-SwiftUIWithCocapodsUITests/Pods-SwiftUIWithCocapods-SwiftUIWithCocapodsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftUIWithCocapods_SwiftUIWithCocapodsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftUIWithCocapods_SwiftUIWithCocapodsUITests
+@end
---
+++ Pods/Target Support Files/Pods-SwiftUIWithCocapodsTests/Pods-SwiftUIWithCocapodsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftUIWithCocapodsTests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftUIWithCocapodsTests
+@end
